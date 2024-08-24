59ed2c3179f0aa335fc286a0feee21dfced63a0c
alexvieru1 <vieru.alexandruu@gmail.com>
2024-08-24 17:40:52 +0300

    fixed photos in hp for lg screens

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "@/components/header";
+import Footer from "@/components/Footer";
 
 const index = () => {
   return (
     <div>
       <Header />
-      <div className="mt-24 text-6xl">contact</div>
+      <div className="mt-24 min-h-screen text-6xl">contact</div>
+      <Footer/>
     </div>
   );
 };
