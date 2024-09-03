f0dc08f0a1bf724bfe77aa3e2095b05e6274946a
alexvieru1 <vieru.alexandruu@gmail.com>
2024-09-03 15:21:25 +0300

    metadata added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,33 @@
 import "./globals.css";
-import { Inter as FontSans } from "next/font/google"
+import { Inter as FontSans } from "next/font/google";
 import Header from "../components/header";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
- 
+
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata = {
   title: "eXpert Photobooth 360",
-  description: "Cabină, oglindă, video 360",
+  description:
+    "Cabină foto, oglindă magică, și video booth 360 pentru evenimente de neuitat. Oferim servicii profesionale de fotografie și video pentru nunți, botezuri, petreceri corporate și alte evenimente speciale.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={cn(
+      <body
+        className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
-        )}>
+        )}
+      >
         <Header />
         <main>{children}</main>
-        <Toaster/>
+        <Toaster />
       </body>
     </html>
   );
