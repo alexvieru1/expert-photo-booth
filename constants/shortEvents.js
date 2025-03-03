2973d30f406e4eace294d6711bbdc79bbd064ad5
Alexandru Vieru <vieru.alexandruu@gmail.com>
2025-03-03 20:09:05 +0200

    all done

--- constants/shortEvents.js
+++ constants/shortEvents.js
@@ -1,34 +1,56 @@
 export const eventPhotos = [
   {
-    title: "Oana's Birthday",
-    date: "Mai 2024",
     src: "/images/events-short/oana2.webp",
   },
   {
-    title: "Botez Rebeca Maria",
-    date: "Mai 2024",
     src: "/images/events-short/rebeca1.webp",
   },
   {
-    title: "Botez Andrei",
-    date: "Iunie 2024",
     src: "/images/events-short/andrei1.webp",
   },
   {
-    title: "Botez Amir",
-    date: "Iunie 2024",
     src: "/images/events-short/amir2.webp",
   },
   {
-    title: "Festivalul de Artă Odisee",
-    date: "August 2024",
+    src: "/images/events-short/1.webp",
+  },
+  {
+    src: "/images/events-short/2.webp",
+  },
+  {
+    src: "/images/events-short/3.webp",
+  },
+  {
+    src: "/images/events-short/4.webp",
+  },
+  {
+    src: "/images/events-short/5.webp",
+  },
+  {
+    src: "/images/events-short/6.webp",
+  },
+  {
     src: "/images/events-short/tghiol2.webp",
   },
   {
-    title: "Botez Cristian-Ioan",
-    date: "August 2024",
     src: "/images/events-short/cristian.webp",
   },
+  {
+    src: "/images/events-short/7.webp",
+  },
+  {
+    src: "/images/events-short/8.webp",
+  },
+  {
+    src: "/images/events-short/9.webp",
+  },
+  {
+    src: "/images/events-short/10.webp",
+  },
+  {
+    src: "/images/events-short/11.webp",
+  },
+  {
+    src: "/images/events-short/12.webp",
+  },
 ];
-
-
